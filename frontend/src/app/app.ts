e4e3e4708fe91f14a6663d6ab2f8b913459ebd6d
GyorkiUr <gyorki.david.laszlo@gmail.com>
2026-03-04 13:09:41 +0100

    Bugfix: Contact list refresh
    Contact list properly initializes on page refresh/load

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { HubspotService } from './services/hubspot';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule], // HttpClientModule törölve innen
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -15,7 +16,10 @@ export class AppComponent implements OnInit {
   formData = { email: '', firstname: '', lastname: '' };
   statusMessage = '';
 
-  constructor(private hubspotService: HubspotService) {}
+  constructor(
+    private hubspotService: HubspotService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit() {
     this.refreshList();
@@ -24,20 +28,31 @@ export class AppComponent implements OnInit {
   refreshList() {
     this.hubspotService.getContacts().subscribe({
       next: (data) => {
-        this.contacts = data.results; // A HubSpot results tömbje
+        this.contacts = data && data.results ? data.results : [];
+        console.log('Lista sikeresen betöltve:', this.contacts);
+        
+        this.cdr.detectChanges();
       },
-      error: (err) => console.error('Hiba a lista betöltésekor', err)
+      error: (err) => {
+        console.error('Hiba a lista betöltésekor:', err);
+        this.statusMessage = 'Hiba történt a lista betöltésekor.';
+      }
     });
   }
 
   onSubmit() {
     this.hubspotService.upsertContact(this.formData).subscribe({
       next: (res) => {
-        this.statusMessage = `Siker! Állapot: ${res.action}, ID: ${res.id}`;
-        this.formData = { email: '', firstname: '', lastname: '' }; // Form ürítése
+        this.statusMessage = `Sikeres mentés! Művelet: ${res.action}, ID: ${res.id}`;
+        
+        this.formData = { email: '', firstname: '', lastname: '' };
+
         this.refreshList();
       },
-      error: (err) => this.statusMessage = 'Hiba történt a mentés során.'
+      error: (err) => {
+        console.error('Mentési hiba:', err);
+        this.statusMessage = 'Hiba történt a mentés során. Ellenőrizd a konzolt!';
+      }
     });
   }
 }
